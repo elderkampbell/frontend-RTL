1b0d0ffc1e2a90d81200eeb4f426af167a1f00d4
elderkampbell <elderkampbell@hotmail.com>
2022-09-20 14:21:52 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+test('Testa se a página contém heading h2 com o texto Page requested not found', () => {
+  renderWithRouter(<NotFound />);
+  const nothingFound = screen.getByRole('heading', { name: /page requested not found/i });
+  expect(nothingFound).toBeInTheDocument();
+});
+
+test('Testa se a página mostra a imagem do pikachu chorando', () => {
+  renderWithRouter(<NotFound />);
+  const imageURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const image = screen.getByRole('img', {
+    name: /pikachu crying because the page requested was not found/i,
+  });
+  expect(image).toHaveProperty('src', imageURL);
+});
