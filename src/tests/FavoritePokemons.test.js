12bc15223d0d367c24a8daa8ba63e3d97b1f3c66
elderkampbell <elderkampbell@hotmail.com>
2022-09-20 14:09:58 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+test('Busca No favorite pokemon found, se não houver pokémons favoritos;', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const noFavs = screen.getByText(/no favorite pokemon found/i);
+  if (noFavs) {
+    expect(noFavs).toBeInTheDocument();
+  } else {
+    const favs = screen.getByText(/average weight:/i);
+    expect(favs.length).toBeGreaterThan(0);
+  }
+});
