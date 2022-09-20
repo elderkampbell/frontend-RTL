95aacaac1c7ea8ecfa1b9659d07e009bac23e852
elderkampbell <elderkampbell@hotmail.com>
2022-09-20 12:09:48 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Testa se o primeiro link deve possuir o texto Home', () => {
+  renderWithRouter(<App />);
+  const home = screen.getByRole('link', { name: /home/i });
+
+  expect(home).toBeInTheDocument();
+});
+
+test('Testa se o primeiro link deve possuir o texto About', () => {
+  renderWithRouter(<App />);
+  const about = screen.getByRole('link', { name: /about/i });
+
+  expect(about).toBeInTheDocument();
+});
+
+test('Testa se o primeiro link deve possuir o texto Favorite Pokémons', () => {
+  renderWithRouter(<App />);
+  const favorite = screen.getByRole('link', { name: /favorite pokémons/i });
+
+  expect(favorite).toBeInTheDocument();
+});
